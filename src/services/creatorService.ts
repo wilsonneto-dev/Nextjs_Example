9868200ecdc25db5106b92b6496beeed61152e57
Wilson Neto <contato@wilsonneto.com.br>
2021-10-26 18:19:07 -0300

    fix: tests

--- src/services/creatorService.ts
+++ src/services/creatorService.ts
@@ -3,22 +3,18 @@ import { api } from './api'
 
 class CreatorsService {
   async get(): Promise<Array<Creator>> {
-    try {
-      const { data } = await api.get<Array<any>>('/authors')
-      return data.map<Creator>((_creator: Creator) => ({
-        description: _creator.description ?? '',
-        online: _creator.online ?? false,
-        displayName: _creator.displayName ?? '',
-        photo: _creator.photo,
-        tags: _creator.tags ?? [],
-        socials: _creator.socials?.map<CreatorSocial>(_social => ({
-          link: _social.link,
-          social: _social.social
-        }))
+    const { data } = await api.get<Array<any>>('/authors')
+    return data.map<Creator>((_creator: Creator) => ({
+      description: _creator.description ?? '',
+      online: _creator.online ?? false,
+      displayName: _creator.displayName ?? '',
+      photo: _creator.photo,
+      tags: _creator.tags ?? [],
+      socials: _creator.socials?.map<CreatorSocial>(_social => ({
+        link: _social.link,
+        social: _social.social
       }))
-    } catch (ex) {
-      return []
-    }
+    }))
   }
 }
 
